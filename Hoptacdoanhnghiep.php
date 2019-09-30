7a025f2eede3407551b22ad3c6251d1387c542d4
= <=>
2019-09-30 22:18:32 +0700

    hop tac doanh nghiep

---
+++ Hoptacdoanhnghiep.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Trường Đại Học Nguyễn Tất Thành</title>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/Hoptacdoanhnghiep.css">
+    
+
+    <link rel="stylesheet" type="text/css" href="css/myCss.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
+    <link rel="shortcut icon" href="http://ntt.edu.vn/web/favicon.ico" type="image/x-icon">
+
+    
+
+</head>
+<?php
+    include "header.php";
+
+?>
+        <div id="main">
+        	<div class="container-fluid">
+        		<div class="row" style="width: 90%;margin: 0px auto;">
+					<div class="col-3">
+						<div class="head">
+							<h3 style="font-size: 14px;">ĐẠI HỌC NGUYỄN TẤT THÀNH</h3>
+							<p style="color: #aa914d"><b>“Đoàn kết - Hội nhập - Năng Động - Trí tuệ - Trách nhiệm”</b></p>
+						</div>
+						<div class="body">
+							<img src="http://ntt.edu.vn/web/upload/images/Hop_tac_doanh_nghiep/htdn.png" alt="">
+							<ul>
+								<li>Với CLB Doanh nghiệp có 650 thành viên thường xuyên hoạt động.</li>
+								<li>05 Câu lạc bộ doanh nghiệp thuộc các khối ngành gồm: Khối Kinh tế, Khối Khoa học kỹ thuật, khối Sức khoẻ.</li>
+								<li>Giới thiệu việc làm cho 6.415 sinh viên hàng năm.</li>
+								<li>Ban Liên lạc Cựu sinh viên với hơn 1000 cựu sinh viên theo các nhóm ngành nghề khác nhau.</li>
+							</ul>
+						</div>
+							
+					</div>
+					<div class="col-9">
+						<div class="img">
+							<img src="http://ntt.edu.vn/web/upload/images/cate_HTDN.jpg" alt="">
+							<article>
+								<h4>Hợp tác doanh nghiệp</h4>
+								<p>NTTU -  Mối quan hệ hợp tác giữa nhà trường và doanh nghiệp là điều có ý nghĩa rất quan trọng. Sự hiệp lực giữa  trường đại học với nhà nước và các doanh nghiệp được coi là động lực cốt yếu của những xã hội và những nền kinh tế dựa trên tri thức.</p>
+								<b><a href="http://vieclam-thuctap.ntt.edu.vn"><i class="fa fa-chevron-circle-right mr-2 c-blue-a5"></i>http://vieclam-thuctap.ntt.edu.vn</a></b>
+							</article>
+						</div>
+						<hr>
+						<div class="main">
+							<img src="http://ntt.edu.vn/web/upload/images/Hop_tac_doanh_nghiep/%5B09-03-2017_15.08.09%5Dngay_hoi_viec_lam.jpg" alt="">
+							<div>
+								<h5><a href="">Giới thiệu việc làm</a></h5>
+								<p>Với phương Châm thực học – Thực hành – Thực danh và Thực nghiệp nhà trường luôn xem trọng công tác tư vấn giới thiệu việc làm cho sinh viên tốt nghiệp thông qua các ngày hội việc làm, Kỹ năng chinh phục nhà tuyển dụng, Bình quân mỗi năm hơn 1500 vị trí việc làm được giới thiệu cho sinh viên. Công tác này hàng năm giúp tăng tỉ lệ sinh viên trường đại học Nguyễn Tất Thành đạt 94,5% sau tốt nghiệp.</p>
+							</div>
+						</div>
+						<hr>
+						<div class="main">
+							<img src="http://ntt.edu.vn/web/upload/images/Tuyen_sinh/Admission_Procedure.jpg" alt="">
+							<div>
+								<h5><a href="">Giới thiệu thực tập</a></h5>
+								<p>Triết lý đào tạo của nhà trường đảm bảo lợi ích của người học, người dạy nhà trường và xã hội thong qua công tác hỗ trợ sinh viên 100% có chỗ thực tập trong quá trình theo học tại trường. Hàng năm hơn 1000 sinh viên được bộ phận Quan hệ doanh nghiệp – Phòng Công tác Sinh viên giới thiệu đến cộng đồng doanh nghiệp giúp sinh viên rút ngắn khoảng cách giữa lý thuyết và thực hành.</p>
+							</div>
+						</div>
+						<hr>
+						<div class="main">
+							<img src="http://ntt.edu.vn/web/upload/images/Hop_tac_doanh_nghiep/clb_doanhnghiep.png" alt="">
+							<div>
+								<h5><a href="">Câu lạc bộ Doanh nghiệp</a></h5>
+								<p>Câu lạc bộ doanh nghiệp trường Đại học Nguyễn Tất Thành được thành lập từ năm 2008 với hơn 650 doanh nghiệp thành viên đa dạng ngành nghề phù hợp với khối ngành đào tạo của nhà trường thường xuyên hỗ trợ góp ý chương trình đào tạo Đại học Nguyễn Tất Thành.</p>
+							</div>
+						</div>
+						<hr>
+						<div class="main">
+							<img src="http://ntt.edu.vn/web/upload/images/Khoa-nganh/khoahoccoban_thumb.png" alt="">
+							<div>
+								<h5><a href="">Ban liên lạc Cựu sinh viên</a></h5>
+								<p>Ban liên lạc cựu sinh viên được hình thành trên nền tảng ý nghĩa thể hiện sự quan tâm của nhà trường dành cho sinh viên sau khi theo học tại trường đồng thời cùng các sinh viên đã thành đạt tiếp tục giúp đỡ các sinh viên đang theo học thong qua các hoạt động quay về trường tuyển dụng các sinh viên khoá sau, hỗ trợ tiếp nhận thực tập sinh viên mới.</p>
+							</div>
+						</div>
+						<hr>
+						<div class="main">
+							<img src="http://ntt.edu.vn/web/upload/images/Hop_tac_doanh_nghiep/htdn.png" alt="">
+							<div>
+								<h5><a href="">Liên kết - Hợp tác</a></h5>
+								<p>Công tác đào tạo của nhà trường luôn có sự đồng hành gắn bó của công đồng doanh nghiệp thong qua sự hợp tác toàn diện giữa nhà trường với các doanh nghiệp lớn như: Cty Cổ phần Sữa Việt Nam Vinamilk, Vietnam Airlines, Sam Sung, Ngân hàng Sacombank… trong việc cung cấp nguồn nhân lực và hợp tác nghiên cứu khoa học…</p>
+							</div>
+						</div>
+					</div>
+				</div>
+        	</div>
+        </div>
+        <br><br>
+<?php 
+    include_once 'footer.php';
+?>
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+<script type="text/javascript">
+    
+        count = 0;
+        wordsArray = ["0902.298.300 - 0906.298.300","0912.298.300  - 0914.298.300"];
+         setInterval(function () {
+            count++;
+            $("#hotline-1").slideUp(600, function () {
+                // odd % 2 == 1
+              $(this).text(wordsArray[count % wordsArray.length]).slideDown(200);
+              console.log('wordsArray '+count % wordsArray.length+' count '+count+' length '+wordsArray.length);
+            });
+          }, 3000);
+</script>
