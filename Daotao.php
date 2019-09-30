23b8bef01062b6ad06a1040b443c180f8ced64bf
anhnt72 <anhnt72@wru.vn>
2019-09-30 22:23:49 +0700

    dao tao

---
+++ Daotao.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Trường Đại Học Nguyễn Tất Thành</title>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/daotao.css">
+    
+
+    <link rel="stylesheet" type="text/css" href="css/myCss.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
+    <link rel="shortcut icon" href="http://ntt.edu.vn/web/favicon.ico" type="image/x-icon">
+
+    
+
+</head>
+<?php
+    include "header.php";
+
+?>
+        <div id="main">
+        	<div class="container-fluid">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="head">
+                            <h3 style="font-size: 14px;">ĐẠI HỌC NGUYỄN TẤT THÀNH</h3>
+                            <p style="color: #aa914d"><b>“Đoàn kết - Hội nhập - Năng Động - Trí tuệ - Trách nhiệm”</b></p>
+                        </div>
+                        <div class="body">
+                            <img src="http://ntt.edu.vn/web/upload/images/Dao_tao/academics_thumb.png" alt="">
+                            <br><br>
+                            <h3>QUY MÔ ĐÀO TẠO</h3>
+                            <ul><li>Hơn 20.000 sinh viên</li></ul>
+                            <h3>ĐỘI NGŨ GIẢNG VIÊN</h3>
+                            <ul>
+                                <li>924 giảng viên</li>
+                                <li>90% có bằng TS, ThS  </li>
+                            </ul>
+                            <h3>CƠ SỞ VẬT CHẤT</h3>
+                            <ul>
+                                <li>4 cơ sở đào tạo</li>
+                                <li>100.000 m² sàn xây dựng</li>
+                                <li>3.000 máy tính</li>
+                                <li>50.000 bản sách</li>
+                                <li>Thư viện đạt chuẩn Quốc gia</li>
+                            </ul>
+                            <h3>CHẤT LƯỢNG</h3>
+                            <ul>
+                                <li>Đạt chuẩn kiểm định chất lượng của Bộ GD&ĐT</li>
+                                <li>Đạt chuẩn QS-Stars 3 sao (Anh Quốc)</li>
+                            </ul>
+                            <h3>KHOA</h3>
+                            <ul>
+                                <li>Y</li>
+                                <li>Dược</li>
+                                <li>Quản trị kinh doanh</li>
+                                <li>Khoa luật</li>
+                                <li>Tài chính-Kế toán</li>
+                                <li>Cơ khí-Điện tử-Ô tô</li>
+                                <li>Kỹ thuật Thực phẩm và Môi trường</li>
+                                <li>Công nghệ Sinh học</li>
+                                <li>Công nghệ thông tin</li>
+                                <li>Kiến trúc-Xây dựng-Mỹ thuật ứng dụng</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-9">
+                        <div class="img">
+                            <img src="http://ntt.edu.vn/web/upload/images/Dao_tao/academics_banner.png" alt="">
+                            <article>
+                                <h4>Đào tạo</h4>
+                                <p>NTTU - Là trường nằm trong doanh nghiệp, ĐH Nguyễn Tất Thành hiểu được quy luật tất yếu của công tác đào tạo là phải đảm bảo được yêu cầu của doanh nghiệp và đáp ứng được nhu cầu của xã hội, trong đó chuẩn đầu ra được xem là quy luật trọng yếu. Theo đó, sinh viên ra trường phải đạt được tiêu chuẩn cần thiết về kiến thức, kỹ năng và thái độ. Tùy vào mỗi ngành học, sinh viên sẽ có những yêu cầu về chuẩn ngoại ngữ, tin học, chuẩn kỹ năng và bổ sung nhiều kiến thức khác nhau.</p>
+                                <b><a href=""><i class="fa fa-chevron-circle-right mr-2 c-blue-a5"></i>http://phongdaotao.ntt.edu.vn/</a></b>
+                            </article>
+                        </div>
+                        <hr>
+                        <div class="main">
+                            <img src="http://ntt.edu.vn/web/upload/images/khoahocsuckhoe.jpg" alt="">
+                            <div>
+                                <h5><a href="">Khối Khoa học sức khỏe</a></h5>
+                                <p>NTTU - Là trọng tâm của Chiến lược phát triển ĐH Nguyễn Tất Thành, khối khoa học sức khỏe gồm khoa Y, khoa Dược và khoa Điều dưỡng, đào tạo các ngành khoa học và chăm sóc sức khỏe bao gồm Dược học, Điều dưỡng, Kỹ thuật y học v.v. Thông qua với việc hợp tác với gần 100 bệnh viện và công ty dược hàng đầu trong cả nước, sinh viên Trường có cơ hội thực tập trong môi trường thực tế ngay trong quá trình đào tạo.</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="main">
+                            <img src="http://ntt.edu.vn/web/upload/images/Khoa-nganh/kinhte_quantri.png" alt="">
+                            <div>
+                                <h5><a href="">Khối Kinh tế - Quản trị</a></h5>
+                                <p>NTTU - Khối Kinh tế - Quản trị bao gồm khoa Quản trị kinh doanh, Luật kinh tế và Tài chính – Kế toán. Tận dụng thế mạnh là trường trong doanh nghiệp, là thành viên của Tập đoàn Dệt May Việt Nam, thành viên khối viện – trường Bộ Công thương, việc đào tạo tại các khoa trong khối kinh tế - quản trị luôn gắn liền với đơn vị sử dụng lao động, giúp sinh viên sớm có kinh nghiệm thực tiễn ngay trong quá trình học.</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="main">
+                            <img src="http://ntt.edu.vn/web/upload/images/Khoa-nganh/kythuat_congnghe.png" alt="">
+                            <div>
+                                <h5><a href="">Khối Kỹ thuật - Công nghệ</a></h5>
+                                <p>NTTU - Với 4 khoa, Khối kỹ thuật – công nghệ không chỉ lớn về số lượng sinh viên mà còn mạnh về chất lượng đào tạo. Sinh viên tốt nghiệp kỹ thuật – công nghệ của Trường đều có việc làm trong vòng 6 tháng sau khi tốt nghiệp. Giảng viên các khoa kỹ thuật – công nghiệp hiện đang chủ nhiệm nhiều đề tài cấp nhà nước, cấp bộ về công nghệ thông tin, cơ khí, điện, điện tự, nông nghiệp, hóa.</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="main">
+                            <img src="http://ntt.edu.vn/web/upload/images/Khoa-nganh/khxh-nv-thumb.png" alt="">
+                            <div>
+                                <h5><a href="">Khối Khoa học xã hội - Nhân văn</a></h5>
+                                <p>NTTU - Khối khoa học xã hội – nhân văn phụ trách đào tạo các ngành ngoại ngữ (tiếng Anh, tiếng Trung, tiếng Nhật, tiếng Hàn), ngành Việt Nam học, các ngành Quản trị nhà hàng, khách sạn, du lịch lữ hành. Việc đào tạo các ngành trong khối khoa học xã hội – nhân văn đều theo định hướng ứng dụng, trên cơ sở chuẩn năng lực mà người học cần có để làm việc hiệu quả trong công việc tương lai.</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="main">
+                            <img src="http://ntt.edu.vn/web/upload/images/Khoa-nganh/mythuat-nghethuat.jpg" alt="">
+                            <div>
+                                <h5><a href="">Khối mỹ thuật - Nghệ thuật</a></h5>
+                                <p>NTTU - Khối Mỹ thuật – Nghệ thuật gồm 2 khoa: Khoa Kiến trúc – Xây dựng – Mỹ thuật ứng dụng và Khoa Âm nhạc. Trong đó, Khoa Kiến trúc – Xây dựng – Mỹ thuật ứng dụng là sự kết hợp giữa mỹ thuật, ứng dụng và kỹ thuật nhằm đào tạo đội ngũ kiến trúc sư, chuyên viên thiết kế nội thất, kỹ sư xây dựng phục vụ các công trình xây dựng. Ngoài ra, các khối Mỹ thuật – Nghệ thuật còn đào tạo các ngành thanh nhạc, đạo diễn, diễn viên v.v.</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="main">
+                            <img src="http://ntt.edu.vn/web/upload/images/Dao_tao_quoc_te/Trang%20Dao%20Tao.JPG" alt="">
+                            <div>
+                                <h5><a href="">Khối đào tạo quốc tế</a></h5>
+                                <p>Được thành lập vào năm 2008, Viện Đào Tạo Quốc Tế NTT (NIIE) mang đến cho sinh viên một môi trường học tập quốc tế chuyên nghiệp, năng động và sáng tạo với hai hệ chương trình: chương trình liên kết Pearson BTEC HND và chương trình chuẩn quốc tế. NIIE tự hào là học viện đầu tiên tại Việt Nam được công nhận kiểm định bởi tổ chức ASIC (Anh Quốc) và trở thành thành viên chính thức của mạng lưới du học QSIAN. Với tấm bằng tốt nghiệp tại NIIE, sinh viên tự tin việc làm tại các doanh nghiệp trong và ngoài nước, đồng thời dễ dàng tìm kiếm cơ hội học cao hơn tại các trường đại học uy tín thế giới. • Chương trình liên kết • Chương trình chuẩn quốc tế</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+        </div>
+        <br><br>
+<?php 
+    include_once 'footer.php';
+?>
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+<script type="text/javascript">
+    
+        count = 0;
+        wordsArray = ["0902.298.300 - 0906.298.300","0912.298.300  - 0914.298.300"];
+         setInterval(function () {
+            count++;
+            $("#hotline-1").slideUp(600, function () {
+                // odd % 2 == 1
+              $(this).text(wordsArray[count % wordsArray.length]).slideDown(200);
+              console.log('wordsArray '+count % wordsArray.length+' count '+count+' length '+wordsArray.length);
+            });
+          }, 3000);
+</script>
